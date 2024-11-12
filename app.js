b3473777bc7e6892599aa29f385c1c00f0041a9c
Jasurchik66 <Jasurchikpubgm@gmail.com>
2024-11-12 10:26:45 +0500

    Ob-Havo malumoti

--- app.js
+++ app.js
@@ -1,48 +1,27 @@
-// 1 - masala
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
 
+const app = express();
+const PORT = 3000;
 
-async function getRandomNumber() {
-    return new Promise((resolve) => {
-        const randomNumber = Math.floor(Math.random() * 10);
-        console.log("Kuting...");
-        setTimeout(() => {
-            resolve(randomNumber);
-        }, 2000)
-    })
-}
+// Foydalanuvchi interfeysi uchun fayllarni o'qish
+app.use(express.static('public'));
 
+// Ob-havo ma'lumotlarini olish uchun endpoint
+app.get('/weather', async (req, res) => {
+    const city = req.query.city;
+    const apiKey = process.env.API_KEY;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-async function random_generator() {
-    console.log(await getRandomNumber());
-}
+    try {
+        const response = await axios.get(url);
+        res.json(response.data);
+    } catch (error) {
+        res.json({ error: 'Shahar topilmadi!' });
+    }
+});
 
-random_generator();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.listen(PORT, () => {
+    console.log(`Server ${PORT} portda ishlamoqda`);
+});
